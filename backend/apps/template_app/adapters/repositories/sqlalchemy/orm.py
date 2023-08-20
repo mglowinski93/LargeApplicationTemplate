df8c24d261a6df7bc2a639b2fdd4a5ad231abbf3
Mateusz Głowiński <mateusz.glowinski@makimo.pl>
2023-08-20 08:41:02 +0200

    Adjust mapping

--- backend/apps/template_app/adapters/repositories/sqlalchemy/orm.py
+++ backend/apps/template_app/adapters/repositories/sqlalchemy/orm.py
@@ -1,14 +1,16 @@
 from sqlalchemy import Table, Column, DateTime, String, UUID
+from sqlalchemy.orm import composite
 
 from apps.common.database import metadata, mapper_registry
 from ....domain.entities import Template
+from ....domain.value_objects import TemplateValue
 
 
 templates = Table(
     "templates",
     metadata,
     Column("id", UUID, primary_key=True),
-    Column("value", String(255)),
+    Column("value_data", String(255)),
     Column("timestamp", DateTime),
 )
 
@@ -18,4 +20,8 @@ def start_mappers():
     # https://docs.sqlalchemy.org/en/20/orm/mapping_styles.html#imperative-mapping
     # and
     # https://docs.sqlalchemy.org/en/20/orm/composites.html#sqlalchemy.orm.composite.
-    mapper_registry.map_imperatively(Template, templates)
+    mapper_registry.map_imperatively(
+        class_=Template,
+        local_table=templates,
+        properties={"_value": composite(TemplateValue, templates.c.value_data)},
+    )
