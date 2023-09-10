f1d0d85789969cc932be777eb54427fb8c98c05d
Mateusz Głowiński <mateusz.glowinski@makimo.pl>
2023-09-10 11:47:40 +0200

    Add queries tests

---
+++ backend/apps/tests/test_template_app/test_services/test_template_queries.py
@@ -0,0 +1,55 @@
+from typing import Callable
+
+import pytest
+
+from apps.template_app.domain.entities import Template as TemplateEntity
+from apps.template_app.domain.ports.exceptions import TemplateDoesNotExist
+from apps.template_app.services import get_template, list_templates
+from apps.template_app.services.mappers import map_template_entity_to_output_dto
+from ..factories import fake_template_id
+
+
+def test_get_template_returns_output_dto_when_template_exists(
+    fake_unit_of_work_factory: Callable,
+    template_entity: TemplateEntity,
+):
+    # Given
+    unit_of_work = fake_unit_of_work_factory(initial_templates=[template_entity])
+
+    # When
+    output_template_dto = get_template(
+        unit_of_work=unit_of_work,
+        template_id=template_entity.id,
+    )
+
+    # Then
+    assert output_template_dto == map_template_entity_to_output_dto(template_entity)
+
+
+def test_get_template_raises_exception_when_requested_template_doesnt_exist(
+    fake_unit_of_work_factory: Callable,
+):
+    with pytest.raises(TemplateDoesNotExist):
+        get_template(
+            unit_of_work=fake_unit_of_work_factory(initial_templates=[]),
+            template_id=fake_template_id(),
+        )
+
+
+def test_list_templates_lists_all_templates(
+    fake_unit_of_work_factory: Callable,
+    template_entity: TemplateEntity,
+):
+    # Given
+    templates = [template_entity]
+    unit_of_work = fake_unit_of_work_factory(initial_templates=templates)
+
+    # When
+    results = list_templates(
+        unit_of_work=unit_of_work,
+    )
+
+    # Then
+    assert results == [
+        map_template_entity_to_output_dto(template) for template in templates
+    ]
--- backend/apps/tests/test_template_app/test_services/test_template_services.py
+++ backend/apps/tests/test_template_app/test_services/test_template_services.py
@@ -6,7 +6,7 @@ from apps.template_app.domain.exceptions import InvalidTemplateValue
 from apps.template_app.domain.entities import Template as TemplateEntity
 from apps.template_app.domain.value_objects import TemplateValue
 from apps.template_app.domain.ports.exceptions import TemplateDoesNotExist
-from apps.template_app.services.template_services import set_template_value
+from apps.template_app.services import create_template, set_template_value
 from ..factories import fake_template_id, fake_template_value
 
 
@@ -66,3 +66,19 @@ def test_set_template_value_raises_exception_when_requested_template_doesnt_exis
         )
 
     assert not unit_of_work.templates.list()
+
+
+def test_create_template_creates_template_with_none_value(
+    fake_unit_of_work_factory: Callable,
+):
+    # Given
+    unit_of_work = fake_unit_of_work_factory(initial_templates=[])
+
+    # When
+    output_template_dto = create_template(
+        unit_of_work=unit_of_work,
+    )
+
+    # Then
+    template = unit_of_work.templates.get(output_template_dto.id)
+    assert template.value is None
