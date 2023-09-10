7d71cd5b4c2fb110b2c2a41a2e04dcc227b63b83
Mateusz Głowiński <mateusz.glowinski@makimo.pl>
2023-09-10 21:12:52 +0200

    Savepoint

--- backend/tests/end_to_end/conftest.py
+++ backend/tests/end_to_end/conftest.py
@@ -1,22 +1,11 @@
 import pytest
-from bootstrap import create_app
-
-from apps.common.database.session import metadata
-from apps.template_app.adapters.repositories.sqlalchemy.orm import (
-    clear_mappers as clear_template_mappers,
-)
 
 
 @pytest.fixture(scope="session")
 def app():
-    yield create_app(environment_name="test")
-    clear_template_mappers()
+    pass
 
 
 @pytest.fixture
 def client(app, prepared_database):
-    metadata.create_all(prepared_database)  # Create the schema in the test database.
-
-    yield app.test_client()
-
-    metadata.drop_all(prepared_database)  # Drop the schema from the test database.
+    pass
