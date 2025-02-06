6575bbeeeb7bf55a23e44d32e2a2bce2151d37bd
BartlomiejJakubczak <bjakubczak96@gmail.com>
2025-02-06 14:43:56 +0100

    moved direct import to above relative one

--- backend/modules/template_module/domain/ports/unit_of_work.py
+++ backend/modules/template_module/domain/ports/unit_of_work.py
@@ -1,6 +1,6 @@
 import abc
+
 from .repository import TemplateRepository
-from modules.common.ports.unit_of_work import AbstractUnitOfWork
 
 
 class AbstractTemplatesUnitOfWork(AbstractUnitOfWork, abc.ABC):
