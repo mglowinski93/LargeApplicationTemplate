15b77a414a811f8fb127b84567d74511b37d407c
BartlomiejJakubczak <36500547+BartlomiejJakubczak@users.noreply.github.com>
2025-02-06 14:55:19 +0100

    Step 4: Add unit of work abstraction (#21)
    * introduced another layer of abstraction AbstractUnitOfWork to AbstractTemplatesUnitOfWork in order to remove unnecessary code. Added __init__.py for said abstraction and added an import of it to AbstractTemplatesUnitOfWork
    
    * reformatted files with black
    
    * moved direct import to above relative one
    
    * bring back annotations import
    
    * Fix import
    
    ---------
    
    Co-authored-by: mateuszglowinski <mglowinski93@gmail.com>

--- backend/modules/template_module/domain/ports/unit_of_work.py
+++ backend/modules/template_module/domain/ports/unit_of_work.py
@@ -1,22 +1,10 @@
 from __future__ import annotations
 
 import abc
+
 from .repository import TemplateRepository
+from ....common.ports.unit_of_work import AbstractUnitOfWork
 
 
-class AbstractTemplatesUnitOfWork(abc.ABC):
+class AbstractTemplatesUnitOfWork(AbstractUnitOfWork, abc.ABC):
     templates: TemplateRepository
-
-    def __enter__(self) -> AbstractTemplatesUnitOfWork:
-        return self
-
-    def __exit__(self, *args):
-        self.rollback()
-
-    @abc.abstractmethod
-    def commit(self):
-        pass
-
-    @abc.abstractmethod
-    def rollback(self):
-        pass
