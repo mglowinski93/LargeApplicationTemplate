17acf13e157074d1925eec3d5b135b18e2316aa9
mateuszglowinski <mglowinski93@gmail.com>
2025-02-06 14:53:02 +0100

    Fix import

--- backend/modules/template_module/domain/ports/unit_of_work.py
+++ backend/modules/template_module/domain/ports/unit_of_work.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
+
 import abc
-from modules.common.ports.unit_of_work import AbstractUnitOfWork
+
 from .repository import TemplateRepository
+from ....common.ports.unit_of_work import AbstractUnitOfWork
 
 
 class AbstractTemplatesUnitOfWork(AbstractUnitOfWork, abc.ABC):
