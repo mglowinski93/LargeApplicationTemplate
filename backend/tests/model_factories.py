ddd742254c4012f3036502045df50feb0d9a5d30
BartlomiejJakubczak <bjakubczak96@gmail.com>
2025-03-31 13:54:26 +0200

    added GenerateDataMixin class for model factories

--- backend/tests/model_factories.py
+++ backend/tests/model_factories.py
@@ -1,6 +1,7 @@
 import inspect
 import sys
-from typing import List, Type
+from functools import partial
+from typing import Any, List, Type
 
 import factory
 
@@ -19,6 +20,49 @@ from modules.template_module.domain.value_objects import (
 from .fakers import fake_template_id, fake_template_value
 
 
+class GenerateDataMixin:
+    @classmethod
+    def generate_data(cls):
+        def convert_dict_from_stub(
+            stub: factory.base.StubObject, factory_: factory.Factory
+        ) -> dict[str, Any]:
+            stub_dict = stub.__dict__
+
+            for key, value in stub_dict.items():
+                if isinstance(value, factory.base.StubObject):
+                    if key in factory_._meta.declarations and isinstance(
+                        factory_._meta.declarations[key], factory.Factory
+                    ):
+                        factory_ = factory_._meta.declarations[key]
+
+                    stub_dict[key] = convert_dict_from_stub(value, factory_)
+                    continue
+
+                if (
+                    key
+                    in [field.name for field in factory_._meta.model._meta.get_fields()]
+                    and factory_._meta.model._meta.get_field(key).choices
+                ):
+                    stub_dict[key] = next(
+                        (
+                            _value
+                            for _key, _value in factory_._meta.model._meta.get_field(
+                                key
+                            ).choices
+                            if value == _key
+                        ),
+                        value,
+                    )
+                    continue
+
+            return stub_dict
+
+        def dict_factory(factory, **kwargs):
+            return convert_dict_from_stub(factory.stub(**kwargs), factory)
+
+        return partial(dict_factory, cls)()
+
+
 class AbstractSQLAlchemyModelFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model: Base | None = None
@@ -26,7 +70,7 @@ class AbstractSQLAlchemyModelFactory(factory.alchemy.SQLAlchemyModelFactory):
         sqlalchemy_session_persistence = "commit"
 
 
-class TemplateFactory(AbstractSQLAlchemyModelFactory):
+class TemplateFactory(GenerateDataMixin, AbstractSQLAlchemyModelFactory):
     class Meta:
         model = SqlAlchemyTemplateDb
 
@@ -40,10 +84,9 @@ class TemplateFactory(AbstractSQLAlchemyModelFactory):
 
 def get_model_factories() -> List[Type[AbstractSQLAlchemyModelFactory]]:
     classes = inspect.getmembers(sys.modules[__name__], inspect.isclass)
-    subclasses = [
+    return [
         cls
         for _, cls in classes
         if issubclass(cls, AbstractSQLAlchemyModelFactory)
         and cls is not AbstractSQLAlchemyModelFactory
     ]
-    return subclasses
