4415637e490659d2be6ef383f60bce859beee18f
BartlomiejJakubczak <bjakubczak96@gmail.com>
2025-03-31 11:35:58 +0200

    fix alembic migration file

--- backend/migrations/alembic/versions/f3612e1e3522_initial.py
+++ backend/migrations/alembic/versions/f3612e1e3522_initial.py
@@ -1,7 +1,6 @@
 """initial
 
 Revision ID: f3612e1e3522
-Revises: bf5ce8ff3ab2
 Create Date: 2025-03-31 09:45:43.358893
 
 """
@@ -9,6 +8,6 @@ from typing import Sequence, Union
 
 # revision identifiers, used by Alembic.
 revision: str = "f3612e1e3522"
-down_revision: Union[str, None] = "bf5ce8ff3ab2"
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
